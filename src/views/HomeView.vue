2dd55c6456b360a6e4ba629a474123a493f9e8d6
Ângelo Lemaire <angelovitor2010@gmail.com>
2023-07-11 21:47:46 -0300

    axios, style input and product and search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,28 @@
 <template>
-  <div >
-
-  </div>
+    <section >
+      <h1>Compre ou Venda</h1>
+      <ProdutosBuscar/>
+      <produtosLista/>
+    </section>
 </template>
 
 <script>
-
+import ProdutosBuscar from '@/components/ProdutosBuscar.vue'
+import ProdutosLista from '@/components/ProdutosLista.vue'
 
 export default {
   name: 'HomeView',
+  components:{
+    ProdutosBuscar,
+    ProdutosLista
+  }
 }
 </script>
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
