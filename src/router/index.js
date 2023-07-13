460083513748e010dd289f68adb8ff2848c3f02d
Ângelo Lemaire <angelovitor2010@gmail.com>
2023-07-12 23:27:29 -0300

    Filter Product

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Produto from '../views/ProdutoId.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/produto/:id',
+    name: 'Produto',
+    component: Produto,
+    props: true
   },
 ]
 
